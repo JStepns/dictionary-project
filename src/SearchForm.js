e450ff2609e451706eb2f9f52fdf8d61b84ea399
Jacqueline Stephens <jacqui.stephens3@gmail.com>
2021-06-17 16:20:55 +1000

    Installed axios & integrated the search function with a dictionary API

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchForm.css";
 
 export default function SearchForm() {
     let [searchWord, setSearchWord] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
     function search(event) {
         event.preventDefault();
         alert(`Searching for the definition of ${searchWord}`);
+
+        //API documentation here:https://dictionaryapi.dev/
+        
+        let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${searchWord}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleSearchWordChange(event) {
