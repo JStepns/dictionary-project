9716695e32de72169f39aea23db54e9343925aab
Jacqueline Stephens <jacqui.stephens3@gmail.com>
2021-06-17 12:28:43 +1000

    Installed bootstrap and wrapped the App in a container

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="Container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Dictionary</h1>
       </header>
+      </div>
     </div>
   );
 }
