c621669c7ab3fa12f43abd1bc45ad040c24b7d19
Jacqueline Stephens <jacqui.stephens3@gmail.com>
2021-06-17 12:07:36 +1000

    Added logo and heading
    Deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Dictionary</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
