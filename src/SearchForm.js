af16c8e0c0543d382a012a0a24969c172e1d8f72
JStepns <73682662+JStepns@users.noreply.github.com>
2021-07-16 16:52:29 +1000

    Created Photos component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./SearchForm.css";
 
 export default function SearchForm(props) {
     let [searchWord, setSearchWord] = useState(props.defaultSearchWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos)
+
+    }
+
     function search() {
-        //API documentation here:https://dictionaryapi.dev/
+        //Dictionary API documentation here:https://dictionaryapi.dev/
         
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${searchWord}`;
-        axios.get(apiUrl).then(handleResponse)
+        axios.get(apiUrl).then(handleDictionaryResponse)
+
+        //Pexels API documentation here:https://www.pexels.com/api/documentation/ 
+
+        let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${searchWord}&per_page=6`;
+        let pexelsApiKey= "563492ad6f9170000100000162411f52d9994660b045a680c52444df";
+        let headers= {Authorization : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -44,6 +58,7 @@ export default function SearchForm(props) {
                 </form>
             </section>
             <Results results={results}/>
+            <Photos photos={photos}/>
         </div>
     );
     } else {
